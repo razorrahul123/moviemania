02df5ccef3a99418868a8d43d46a8dca3d294879
razorrahul123 <rahul.kr.sushant@gmail.com>
2016-09-02 15:58:16 +0530

    Average Rating and User Authentication

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,6 +1,8 @@
 class ReviewsController < ApplicationController
   before_action :find_play
   before_action :find_review, only: [:edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:new, :edit]
+
 
   def new
     @review = Review.new
